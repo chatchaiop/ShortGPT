c1ccd20b5dba530d9a135a0f3e6a5e76ffec117b
chatchaiop <71642552+chatchaiop@users.noreply.github.com>
2023-07-25 10:51:21 +0700

    Update content_video_engine.py

--- shortGPT/engine/content_video_engine.py
+++ shortGPT/engine/content_video_engine.py
@@ -132,7 +132,7 @@ class ContentVideoEngine(AbstractContentEngine):
                 caption_type = EditingStep.ADD_CAPTION_LANDSCAPE_ARABIC if self._db_language == Language.ARABIC.value else EditingStep.ADD_CAPTION_LANDSCAPE 
             
             for (t1, t2), text in self._db_timed_captions:
-                videoEditor.addEditingStep(caption_type, {'text': '',
+                videoEditor.addEditingStep(caption_type, {'text': 'z',
                                                                      'set_time_start': t1,
                                                                      'set_time_end': t2})
     
